a1863a08db4a367fce1336ccb8ad67283ceb8608
blaze <kontaktAK@Yandex.ru>
2019-07-29 11:26:59 +0300

    prepare heroku deploy

---
+++ public/precache-manifest.e9c9dd6f3e037ec01a4b16b0bd61c9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62729697bee18384dd6481a7fd3fd58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8fdbd6796ec01d1b627",
+    "url": "/static/css/main.8f7eec71.chunk.css"
+  },
+  {
+    "revision": "d3732fb3008ffc3f083f",
+    "url": "/static/js/2.75bebbc0.chunk.js"
+  },
+  {
+    "revision": "e8fdbd6796ec01d1b627",
+    "url": "/static/js/main.3909a9df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.jpg"
+  }
+]);
