ae4cd3109e82879580a88eb6892ebd57aca1eb05
blaze <kontaktAK@Yandex.ru>
2019-07-29 11:24:54 +0300

    prepare heroku deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express     = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema      = require('./schema')
 const cors        = require('cors')
+const path        = require('path')
 
 const app = express()
 // allow cross-origin
@@ -14,6 +15,12 @@ app.use(
     graphiql: true,
   }),
 )
+
+app.use(express.static('public'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
